02789f1887319bd149626ad37c30c165a100c6db
Leandro Haruki <leandroharuki@gmail.com>
2019-08-19 23:01:42 -0300

    Let the s*** show start c:

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { SafeAreaView, StyleSheet, ScrollView, View, Text, StatusBar } from 'react-native';
+import Home from './src/scenes/home';
+
+const App = () => {
+  return (
+    <>
+      <Home />
+    </>
+  );
+};
+
+export default App;
